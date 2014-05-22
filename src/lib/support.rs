901aa04618a000f0f4300f8b33c8df74e102498d
Ben Harris <mail@bharr.is>
2014-05-22 17:32:21 +0800

    Fix build errors. Also add building of STM32 platform

--- src/lib/support.rs
+++ src/lib/support.rs
@@ -75,6 +75,17 @@ pub fn abort() -> ! {
   loop {}
 }
 
+#[lang = "stack_exhausted"]
+extern fn stack_exhausted() { /* ... */ }
+
+#[lang = "eh_personality"]
+extern fn eh_personality() { /* ... */ }
+
+#[lang = "begin_unwind"]
+extern fn begin_unwind() { 
+  abort();
+}
+
 #[no_split_stack]
 #[no_mangle]
 pub fn __aeabi_unwind_cpp_pr0() {
